6a994d52ac6b64ea0e5a6f38790bc0361a087252
Sean Dan <seandan@seans-mbp.myfiosgateway.com>
2021-01-21 22:21:07 -0500

    added face down functionality

--- frontend/src/util/card.js
+++ frontend/src/util/card.js
@@ -1,7 +1,21 @@
+import { Box } from '@chakra-ui/react';
 import PlayingCard from '../components/Card/PlayingCard';
 
 export const formatCard = (card) => {
-  console.log({ card });
+  const cardArr = card.card.split(' ');
+  const value = cardArr[0];
+  const suit = cardArr[1];
+  return <PlayingCard value={value} suit={suit} />;
+};
+
+export const formatDealerCard = (card, idx) => {
+  if (idx === 0) {
+    return (
+      <Box w="100px" h="200px" bg="#9F7AEA">
+        FACE DOWN CARD
+      </Box>
+    );
+  }
   const cardArr = card.card.split(' ');
   const value = cardArr[0];
   const suit = cardArr[1];
